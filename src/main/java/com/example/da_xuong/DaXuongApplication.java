b46edcb80b5b4cf0443b60fba593898c58bbfcf3
Ph54005 <zennodeptrai@gmail.com>
2025-05-22 21:14:21 +0700

    commit

---
+++ src/main/java/com/example/da_xuong/DaXuongApplication.java
@@ -0,0 +1,13 @@
+package com.example.da_xuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaXuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaXuongApplication.class, args);
+    }
+
+}
